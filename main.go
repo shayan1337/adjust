7688e9794244f106e789e11b5395b932961e68aa
Sayan <shayan1337@gmail.com>
2019-07-10 19:46:42 +0530

    Parse 'parallel' flag from command line and throttle request with it

--- main.go
+++ main.go
@@ -4,20 +4,28 @@ import (
 	. "adjust/app"
 	. "adjust/hasher"
 	. "adjust/provider"
+	"flag"
 	"log"
 	"os"
+	"runtime"
 )
 
 func main() {
 
+	throttleRequest()
+	commandLineArgs := getCommandLineArgs()
+
 	hasher := NewHash()
 	logger := getLogger()
 	provider := NewHttpProvider()
-	commandLineArgs := getCommandLineArgs()
 
-	app := NewApp(hasher, provider, logger)
+	NewApp(hasher, provider, logger).Fetch(commandLineArgs)
+}
 
-	app.Fetch(commandLineArgs[1:])
+func throttleRequest() {
+	maxParallelRequests := flag.Int("parallel", 10, "a flag to limit number of parallel requests")
+	flag.Parse()
+	runtime.GOMAXPROCS(*maxParallelRequests)
 }
 
 func getCommandLineArgs() []string{
